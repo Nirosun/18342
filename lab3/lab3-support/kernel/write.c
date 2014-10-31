dc5ac523666bb33a7d6ca707c7ca514991693e97
wy4515 <y.wu4515@gmail.com>
2014-10-31 12:29:25 -0700

    move kernel files from lab2 to lab3

---
+++ lab3/lab3-support/kernel/write.c
@@ -0,0 +1,36 @@
+/*
+ * write.c: Handle write syscall.
+ *
+ * Author: Zhengyang Zuo <zzuo@andrew.cmu.edu>
+ * Date:   Sun, 19 Oct 2014 08:52 PM
+ */
+
+#include <exports.h>
+#include <bits/errno.h>
+#include <bits/fileno.h>
+
+ssize_t write (int fd, const void *buf, size_t count)
+{
+	char *cbuf = (char *)buf;
+	int i;
+
+	if (fd != STDOUT_FILENO)
+	{
+		printf("Error: wrong file number\n");
+		return -EBADF;
+	}
+	if ((unsigned)cbuf < 0xa0000000 
+		|| ((unsigned)cbuf + count > 0xa3ffffff))
+	{
+		printf("Error: bad address\n");		
+		return -EFAULT;
+	}
+
+	for (i = 0; i < count && cbuf[i] != '\0'; i ++)
+	{
+		putc(cbuf[i]);
+	}
+	putc('\n');
+
+	return i;
+}
