a8047f27ef50a5700e2acef44beb3fb624a1f32e
Nirosun <zuozhengyang@gmail.com>
2014-10-31 14:58:41 -0400

    Permission change

---
+++ lab2/part2/kernel/include/bits/addrno.h
@@ -0,0 +1,15 @@
+/*
+ * addrno.h: Defines address constants
+ *
+ * Author: Zhengyang Zuo <zzuo@andrew.cmu.edu>
+ * Date:   Sun, 19 Oct 2014 08:24 PM
+ */
+
+#ifndef BITS_ADDRNO_H
+#define BITS_ADDRNO_H
+
+#define KERNEL_ADDR	0xa3000000
+#define USER_ADDR	0xa2000000
+#define OLDSP_ADDR	0xa1000000
+
+#endif /* BITS_ADDRNO_H */
