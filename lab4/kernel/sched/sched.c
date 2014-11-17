ad6d6bce693ab911a866ee276f94cf007364bc15
wy4515 <y.wu4515@gmail.com>
2014-11-16 22:57:35 -0800

    clean up

--- lab4/kernel/sched/sched.c
+++ lab4/kernel/sched/sched.c
@@ -25,21 +25,21 @@ tcb_t system_tcb[OS_MAX_TASKS]; /*allocate memory for system TCBs */
  * @brief set up the tcb for the ith task
  */
 void setup_task_context(task_t *task, tcb_t *tcb, uint8_t prio) {
-    sched_context_t *context;
+    sched_context_t *context = &(tcb->context);
 
-    tcb->native_prio = prio;
-    tcb->cur_prio = prio;
-    context = &(tcb->context);
-    context->r4 = (uint32_t)task->lambda;
-    context->r5 = (uint32_t)task->data;
-    context->r6 = (uint32_t)task->stack_pos;
-    context->r7 = 0;
-    context->r8 = global_data;
-    context->r9 = 0;
+    context->r4  = (uint32_t)task->lambda;
+    context->r5  = (uint32_t)task->data;
+    context->r6  = (uint32_t)task->stack_pos;
+    context->r7  = 0;
+    context->r8  = global_data;
+    context->r9  = 0;
     context->r10 = 0;
     context->r11 = 0;
-    context->sp = (void *)tcb->kstack_high;
-    context->lr = launch_task;
+    context->sp  = (void *)tcb->kstack_high;
+    context->lr  = launch_task;
+    
+    tcb->native_prio = prio;
+    tcb->cur_prio = prio;
     tcb->holds_lock = 0;
     tcb->sleep_queue = NULL;
 }
@@ -60,27 +60,27 @@ static void __attribute__((unused)) idle(void)
 
 void sched_init(task_t* main_task  __attribute__((unused)))
 {
-    // Set up tcb for IDLE task
-    sched_context_t* context;
+    tcb_t *idle_tcb = &(system_tcb[IDLE_PRIO]);
+    sched_context_t *context = &(idle_tcb->context);
 
-    system_tcb[IDLE_PRIO].native_prio = IDLE_PRIO;
-    system_tcb[IDLE_PRIO].cur_prio = IDLE_PRIO;
-    context = &(system_tcb[IDLE_PRIO].context);
-    context->r4 = (uint32_t)idle;
-    context->r5 = (uint32_t)NULL;
-    context->r6 = (uint32_t)NULL;
-    context->r7 = 0;
-    context->r8 = global_data;
-    context->r9 = 0;
+    context->r4  = (uint32_t)idle;
+    context->r5  = (uint32_t)NULL;
+    context->r6  = (uint32_t)NULL;
+    context->r7  = 0;
+    context->r8  = global_data;
+    context->r9  = 0;
     context->r10 = 0;
     context->r11 = 0;
-    context->sp = system_tcb[IDLE_PRIO].kstack_high;
-    context->lr = launch_task;
-    system_tcb[IDLE_PRIO].holds_lock = 0;
-    system_tcb[IDLE_PRIO].sleep_queue = NULL;
+    context->sp  = idle_tcb->kstack_high;
+    context->lr  = launch_task;
+
+    idle_tcb->native_prio = IDLE_PRIO;
+    idle_tcb->cur_prio = IDLE_PRIO;
+    idle_tcb->holds_lock = 0;
+    idle_tcb->sleep_queue = NULL;
 
     // Add the IDLE task to run queue
-    runqueue_add(&system_tcb[IDLE_PRIO], IDLE_PRIO);
+    runqueue_add(idle_tcb, IDLE_PRIO);
 }
 
 /**
